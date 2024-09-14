55a96433fcc524815430cb9d9b321f845a60ee48
GOWSHIK <gowshikofficial2022@gmail.com>
2024-09-14 22:26:21 +0530

    ADDED G.O.A.T

---
+++ src/main/java/vijaypkg/vijayClass.java
@@ -0,0 +1,10 @@
+package vijaypkg;
+
+public class vijayClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Gowshik-2004");
+		System.out.println("The Greatest Of All Time");
+	}
+
+}
